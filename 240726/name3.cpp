00aeaf4cecda88de7071de0b3004f8a458fca893
madhuraa18 <mungekarmadhura18@gmail.com>
2026-07-24 06:59:13 +0000

    switch

--- 240726/name3.cpp
+++ 240726/name3.cpp
@@ -10,7 +10,19 @@ namespace madhura
 
 int main()
 {
-    madhura::add_sub();
-    madhura::mul_div();
+    int choice;
+    int a,b;
+    cout << "\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Modulus\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch(choice) {
+        case 1: cout << "Result = " << a + b; break;
+        case 2: cout << "Result = " << a - b; break;
+        case 3: cout << "Result = " << a * b; break;
+        case 4: cout << "Result = " << (float)a / b; break;
+        case 5: cout << "Result = " << a % b; break;
+        default: cout << "Invalid choice";
+    }
     return 0;
 }
